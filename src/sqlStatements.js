12e439d12c0a70f67194967653d17bfd7b9f2604
Savannah Vel <savannah.vel@vivint.com>
2023-01-31 22:29:52 -0800

    added to and cleaned up sql queries

--- src/sqlStatements.js
+++ src/sqlStatements.js
@@ -1,29 +1,54 @@
-// pull in queries here
-// query strings -- maybe pull these out? 
-const viewAllDepartments = 'SELECT id, name FROM department';
+const viewAllDepartments = 'SELECT id, name as department FROM department';
 
-const viewAllRoles = 'SELECT id, title, salary, department_id FROM role';
+const viewAllRoles = `
+    SELECT r.title, r.id, d.name as department, r.salary from role as r
+    JOIN department as d
+    ON d.id = r.department_id
+`;
 
-const viewAllEmployees = 'SELECT id, first_name, last_name, roll_id, manager_id FROM employee';
+const getRoleId = 'SELECT id FROM role WHERE title = ?';
 
-// const viewAllEmployeesByManager = `SELECT id, first_name, last_name, manager_id FROM employee WHERE manager_id = ${manager_id}`
+const viewAllEmployeesFullData = 
+    `SELECT 
+        e.id, 
+        e.first_name, 
+        e.last_name,
+        r.title,
+        d.name AS department,
+        r.salary,
+        CONCAT(m.first_name," ", m.last_name) AS manager 
+        FROM employee e 
+        JOIN employee m 
+            ON m.id = e.manager_id
+        JOIN role as r
+            ON r.id = e.role_id
+        JOIN department as d
+            ON d.id = r.department_id
+    `
+;
 
-// const viewAllEmployeesByDepartment = 
-//     `select e.first_name, e.last_name, r.title from employee as e
-//     join role as r
-//     on r.id = e.role_id;
-//     join department as d
-//     on d.id = r.department_id
-//     where d.name = ${something}
-//     `
+const viewAllEmployees = 'SELECT id, CONCAT (first_name, " ", last_name) AS full_name, role_id, manager_id FROM employee';
 
-// const viewUtilizedDepartmentBudget = `
-// SELECT d.name, SUM(salary) as totalSalary
-// from role as r
-// join department as d
-// on r.department_id = d.id
-// WHERE d.name = ${something}
-// GROUP BY d.name
+const viewEmployee = `SELECT 
+        e.id, 
+        e.first_name, 
+        e.last_name,
+        r.title,
+        d.name AS department,
+        r.salary,
+        CONCAT(m.first_name," ", m.last_name) AS manager 
+        FROM employee e 
+        JOIN employee m 
+            ON m.id = e.manager_id
+        JOIN role as r
+            ON r.id = e.role_id
+        JOIN department as d
+            ON d.id = r.department_id
+        WHERE e.first_name = ?
+        AND e.last_name = ?    
+        `
+
+const viewAllManagers = 'SELECT CONCAT (first_name, " ", last_name, "-", id) AS FULL_NAME FROM employee WHERE manager_id = id';
 
 const addDepartment =
     `INSERT INTO department (name)
@@ -31,21 +56,27 @@ const addDepartment =
 
 const addRole =
     `INSERT INTO role (title, salary, department_id)
-    VALUES (?, ?, ?)`; 
+    VALUES (?, ?, (SELECT id FROM department WHERE name = ?))`; 
 
 const addEmployee =
     `INSERT INTO employee (first_name, last_name, role_id, manager_id)
-    VALUES (?, ?, ?, ?)`;
+    VALUES (?, ?, (SELECT id FROM role WHERE title = ?), ?)`;
 
 const updateEmployeeRole =
-    `UPDATE employee
-    SET role_id = ?,
-    WHERE employee.first_name = ? AND employee.last_name =?`;
+    `UPDATE employee as e
+    JOIN role as r
+    ON e.role_id = r.id
+    SET e.role_id = ?
+    WHERE e.first_name = ? AND e.last_name = ?`
 
 module.exports = {
     viewAllDepartments,
     viewAllRoles,
+    getRoleId,
     viewAllEmployees,
+    viewAllManagers,
+    viewAllEmployeesFullData,
+    viewEmployee,
     addDepartment,
     addRole,
     addEmployee,
